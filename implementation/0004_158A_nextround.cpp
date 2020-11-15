95e3771bf6fbc9bf87a5ec57af5ac18cd6973f5e
johnescobia <escobiajohn@gmail.com>
2020-11-15 17:15:10 +0800

    Clean code and add comments.

--- implementation/0004_158A_nextround.cpp
+++ implementation/0004_158A_nextround.cpp
@@ -5,7 +5,7 @@ int main(){
 	int n=0; // number of participants
 	int k=0; // finisher's place to be compared with
 	
-	cin >> n >> k;
+	cin >> n >> k; // get n and k
 	
 	if(k>=1 && k<=50 && n>=1 && n<=50 && k<=n){
 		int arr[n];  // array of each participant's score
@@ -14,11 +14,10 @@ int main(){
 			cin >> arr[i];
 		}
 		
-		int arr_size=sizeof(arr)/sizeof(arr[0]); // size of array
 		int kScore=arr[k-1];    // k-th place finisher's score
 		int qualified=0;        // count of qualifying contestants
 		
-		for(int i=0; i<arr_size; i++){
+		for(int i=0; i<n; i++){ // get number of qualified participants
 			if(arr[i]>=kScore && arr[i]>0){
 				++qualified;
 			}
