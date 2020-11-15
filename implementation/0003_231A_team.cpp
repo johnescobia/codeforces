77c0d8095eefed6e4ac9130430f93e009291cf63
johnescobia <escobiajohn@gmail.com>
2020-11-15 17:00:06 +0800

    Added comments for better readability. Changed delimiter of for loop from integer c to size of array arr for better maintainability.

--- implementation/0003_231A_team.cpp
+++ implementation/0003_231A_team.cpp
@@ -2,35 +2,44 @@
 using namespace std;
 
 int main(){
-	int n=0, p=0, v=0, t=0;
+	int n=0;  // number of problems in the contest
+	int p=0;  // Petya's certainty
+	int v=0;  // Vasya's certainty
+	int t=0;  // Tonya's certainty
 	
-	cin >> n;
+	cin >> n; // get number of problems
 	
+	// if number of problems is equal to or more than 1 and less than or
+	// equal to 1000
 	if(n>=1 && n<=1000){
-		int arr[n], c=n;
+		int arr[n];  // group's certainty for each problem
+		int c=0;     // to be used for arr index 
+		int total=0; // total certainty
 		
 		while(n>0){
-			cin >> p >> v >> t;
+			cin >> p >> v >> t; // get each person's certainty
+			total = p+v+t;      // get total certainty
 			
-			t = p+v+t;
-			
-			if(t>1){
-				arr[c-n] = 1;
-			}else{
-				arr[c-n] = 0;
+			if(total>1){          // if more than 1 person is certain
+				arr[c] = 1;
+			}else{                // if more than 1 person is uncertain
+				arr[c] = 0;
 			}
 			
-			t=0;
-			--n;
+			total=0; // reinitialize total=0
+			++c;     // increment c by 1
+			--n;     // decrement n by 1
 		}
 		
-		int sum = 0;
+		size_t arrSize = sizeof(arr)/sizeof(arr[0]); // array size
+		int sum = 0; // total problems group is certain
 		
-		for(int i=0; i<c; i++){
+		// get sum of total problems group is certain
+		for(size_t i = 0; i < arrSize; i++){ 
 			sum += arr[i];
 		}
 		
-		cout << sum;
+		cout << sum; // print sum
 		
 	}
 	
