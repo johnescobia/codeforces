a75881740043acdc9d3baaecad71373557fb4bbb
johnescobia <escobiajohn@gmail.com>
2020-11-18 15:09:26 +0800

    Implement 96A

---
+++ difficulty_900/96A_football.cpp
@@ -0,0 +1,45 @@
+// https://codeforces.com/problemset/problem/96/A
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string pos;
+	int count=1, len;
+	
+	cin >> pos;
+	
+	char c = pos.at(0);
+	len = pos.length();
+	
+	for(int i=1; i<len; i++)
+	{
+		if(count < 7)
+		{
+			if(c == pos.at(i))
+			{
+				++count;
+			}
+			else
+			{
+				c = pos.at(i);
+				count = 1;
+			}
+		}
+		else
+		{
+			break;
+		}
+	}
+	
+	if(count < 7)
+	{
+		cout << "NO";
+	}
+	else
+	{
+		cout << "YES";
+	}
+	
+	return 0;
+}
