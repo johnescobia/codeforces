5d0a99305c7addac1538f84e73fa71cc0ca31b1f
johnescobia <escobiajohn@gmail.com>
2020-11-18 10:11:59 +0800

    Implement 791A

---
+++ implementation/0014_791A_bearandbigbrother.cpp
@@ -0,0 +1,46 @@
+/*TASK
+ * Every year weight1 triples and weight2 doubles. At start weight1 is
+ * always lesser than or equal to weight2.
+ * 
+ * Input: weight1 weight2
+ * Output: number of years for weight 1 to exceed weight2
+ * 
+ *EXAMPLES
+ * #1
+ * Input: 4 7
+ * Output: 2
+ * 
+ * #2
+ * Input: 4 9
+ * Output: 3
+ * 
+ * #3
+ * Input: 1 1
+ * Output: 1
+ * */
+ 
+#include <iostream>
+using namespace std;
+
+int main(){
+	int weight1, weight2;
+	
+	cin >> weight1 >> weight2;
+	
+	int year=0;
+	bool CONTINUE=true;
+	
+	while(CONTINUE){
+		++year;
+		weight1 = weight1*3;
+		weight2 = weight2*2;
+		
+		if(weight1>weight2){
+			CONTINUE=false;
+		}
+	}
+	
+	cout << year;
+
+	return 0;
+}
