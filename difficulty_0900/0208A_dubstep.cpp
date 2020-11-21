3e614b093418f61d5974f86831a7d14ecc30f0b1
johnescobia <escobiajohn@gmail.com>
2020-11-21 22:56:09 +0800

    Finish implementing 208A

--- difficulty_0900/0208A_dubstep.cpp
+++ difficulty_0900/0208A_dubstep.cpp
@@ -1,4 +1,9 @@
-// https://codeforces.com/problemset/problem/208/A
+/* Link to problem:
+ * https://codeforces.com/contest/208/problem/A
+ * 
+ * Link to submission with test cases:
+ * https://codeforces.com/contest/208/submission/99129198
+ * */
 #include <iostream>
 #include <vector>
 
@@ -7,13 +12,14 @@ int main()
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	
-	std::string str, subStr="WUB", strCheck;
-	int subStrLen = subStr.length(), difference;
 	std::vector<int> arrPos;
+	std::string str, subStr="WUB", strCheck;
 	
 	std::cin >> str;
 	
-	for(std::size_t i=0; i<str.length(); i++)
+	int strLen = str.length(), subStrLen = subStr.length(), difference;
+	
+	for(int i=0; i<strLen; i++)
 	{
 		strCheck = str.substr(i, subStrLen);
 
@@ -23,16 +29,33 @@ int main()
 		}
 	}
 	
-	for(std::size_t i=0; i<arrPos.size()-1; i++)
+	if(arrPos.size() == 0)
 	{
-		difference = arrPos.at(i+1) - arrPos.at(i);
-
-		if(difference != subStrLen)
-		{
-			std::cout << str.substr(arrPos.at(i)+subStrLen, difference-subStrLen);
-			std::cout << ' ';
-		} 
+		std::cout << str;
 	}
+	else
+	{
+		if(arrPos.at(0) != 0)
+		{
+			std::cout << str.substr(0, arrPos.at(0)) << ' ';
+		}
+		
+		for(std::size_t i=0; i<arrPos.size()-1; i++)
+		{
+			difference = arrPos.at(i+1) - arrPos.at(i);
+
+			if(difference != subStrLen)
+			{
+				std::cout << str.substr(arrPos.at(i)+subStrLen, difference-subStrLen);
+				std::cout << ' ';
+			} 
+		}
+		
+		if(arrPos.back() + subStrLen != strLen)
+		{
+			std::cout << str.substr(arrPos.back()+subStrLen, strLen-1);
+		}
+	}	
 	
 	return 0;
 }
