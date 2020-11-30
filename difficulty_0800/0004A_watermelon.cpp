2dcc56c778b599c9ba162f357695cad021bca00a
John Escobia <escobiajohn@gmail.com>
2020-11-30 23:26:43 +0800

    Write better comments for 4A

--- difficulty_0800/0004A_watermelon.cpp
+++ difficulty_0800/0004A_watermelon.cpp
@@ -1,18 +1,40 @@
+/* DATA TYPES
+ * Input : w     --> int
+ * Output: print --> string of either "YES" or "NO"
+ * 
+ * EXAMPLE
+ * Input : 44
+ * Output: YES
+ * 
+ * LINKS
+ * Problem   : https://codeforces.com/problemset/problem/4/A
+ * Submission: https://codeforces.com/contest/4/submission/98324086
+ * 
+ * VARIABLES
+ * w --> weight of watermelon
+ * f --> weight of watermelon in half
+ * */
+
 #include <iostream>
 #include <cstdlib>
 
-int main () {
-	int w=0; // weight of watermelon
-	int f=0; // weight of watermelon in half
+int main ()
+{
+	int w=0; 
+	int f=0;
 	
 	std::cin >> w;
 	
-	if(w>=1 && w<=100){
+	if(w>=1 && w<=100)
+	{
 		f = w/2; 
 		
-		if(w%2==0 && f>1){
+		if(w%2==0 && f>1)
+		{
 			std::cout << "YES";
-		}else{
+		}
+		else
+		{
 			std::cout << "NO";
 		}
 	}
